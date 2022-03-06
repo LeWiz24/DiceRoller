ca8190e7719409ed5363508ac70bec1a81c9aac5
Mario Olivares <MarioOlivares_95@hotmail.com>
2022-03-06 16:18:33 -0500

    Added images to dice roller

--- app/src/main/java/com/olivares/foundations/MainActivity.kt
+++ app/src/main/java/com/olivares/foundations/MainActivity.kt
@@ -3,52 +3,43 @@ package com.olivares.foundations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Declaring and initiating button to be found by roll_button button
+        // Get Button view from layout and assign a click listener
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
 
-        val countButton: Button = findViewById(R.id.count_up)
-        countButton.setOnClickListener { countUp() }
 
     }
 
         // Adding click handler to display toast when the button is pressed
         private fun rollDice(){
             val randomInt = (1..6).random()
-            val resultText: TextView = findViewById(R.id.result_text)
-            resultText.text = randomInt.toString()
-            Toast.makeText(this, "I love you, Squishy!!!", Toast.LENGTH_SHORT).show()
-
-
-        }
-
-        private fun countUp(){
-            // Tells function the ID of textview to edit
-            val resultText: TextView = findViewById(R.id.result_text)
-            // Condition statement
-            if (resultText.text == "Hello World!") {
-                resultText.text = "1"
-            }else{
-                var resultInt = resultText.text.toString().toInt()
-                if(resultInt<100) {
-                    resultInt ++
-                    resultText.text = resultInt.toString()
-
-
-
-                }
-
+            val drawableResource = when(randomInt){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
             }
+            diceImage.setImageResource(drawableResource)
+
             Toast.makeText(this, "I love you, Squishy!!!", Toast.LENGTH_SHORT).show()
 
+
         }
-    }
+
+        }
+
